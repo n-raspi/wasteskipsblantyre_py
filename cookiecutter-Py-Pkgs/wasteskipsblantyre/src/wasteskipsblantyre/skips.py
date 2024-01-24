93b957d4dfd9116a81716bad4fafcfd26d08abe6
Seemann-Ricard  Nicolas <nseemann@mavt-ghe-n112m.d.ethz.ch>
2024-01-24 14:20:21 +0100

    adding dictionary

--- cookiecutter-Py-Pkgs/wasteskipsblantyre/src/wasteskipsblantyre/skips.py
+++ cookiecutter-Py-Pkgs/wasteskipsblantyre/src/wasteskipsblantyre/skips.py
@@ -5,3 +5,8 @@ def skips():
 	with resources.path("wasteskipsblantyre.data", "skips_November_2023.csv") as f:
 		df = pd.read_csv(f)
 	return df
+
+def dictionary():
+	with resources.path("wasteskipsblantyre.data", "dictionary.csv") as f:
+		df = pd.read_csv(f)
+	return df
